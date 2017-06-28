5a378b38b74ea52773e39982484db65429188bfa
rbreekveldt <rbreekveldt@tmns.com>
2017-06-28 22:27:39 +0200

    Create Test Files Controller

---
+++ src/test/java/ba/pehli/cinema/controller/MovieControllerTest.java
@@ -0,0 +1,79 @@
+package ba.pehli.cinema.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rbreekveldt on 28-6-17.
+ */
+public class MovieControllerTest {
+    @Test
+    public void list() throws Exception {
+    }
+
+    @Test
+    public void showPage() throws Exception {
+    }
+
+    @Test
+    public void downloadImage() throws Exception {
+    }
+
+    @Test
+    public void showEdit() throws Exception {
+    }
+
+    @Test
+    public void showNew() throws Exception {
+    }
+
+    @Test
+    public void edit() throws Exception {
+    }
+
+    @Test
+    public void newMovie() throws Exception {
+    }
+
+    @Test
+    public void rateMovie() throws Exception {
+    }
+
+    @Test
+    public void generateCatalog() throws Exception {
+    }
+
+    @Test
+    public void getInfo() throws Exception {
+    }
+
+    @Test
+    public void connectToFacebook() throws Exception {
+    }
+
+    @Test
+    public void initBinder() throws Exception {
+    }
+
+    @Test
+    public void setMessageSource() throws Exception {
+    }
+
+    @Test
+    public void setEmailUtils() throws Exception {
+    }
+
+    @Test
+    public void setDataSource() throws Exception {
+    }
+
+    @Test
+    public void setRestTemplate() throws Exception {
+    }
+
+    @Test
+    public void setFacebook() throws Exception {
+    }
+
+}
---
+++ src/test/java/ba/pehli/cinema/controller/RestfulWSControllesTest.java
@@ -0,0 +1,23 @@
+package ba.pehli.cinema.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rbreekveldt on 28-6-17.
+ */
+public class RestfulWSControllesTest {
+    @Test
+    public void list() throws Exception {
+    }
+
+    @Test
+    public void getMovieInformation() throws Exception {
+    }
+
+    @Test
+    public void setMovieDao() throws Exception {
+    }
+
+}
